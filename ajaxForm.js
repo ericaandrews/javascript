2beefca1925a2fc736b60bec3fd933b30311ca09
ericaandrews <eureka.web.dev@gmail.com>
2018-09-09 17:38:22 -0700

    Update ajaxForm.js

--- ajaxForm.js
+++ ajaxForm.js
@@ -1,16 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head> 
-</head>
-<body>
-    <p>This webpage contains a form and an Ajax script that will receive data sent from the server. The form contains four input text boxes, for Name, Time, Date&#40;day, date format&#41;, and Date&#40;mm/dd/yy format&#41;. After the user enters their name in the Name text box and then clicks outside this text box, the Ajax script should access three php scripts on the server which will contain the Time, Date&#40;day, date format&#41;, and Date&#40;mm/dd/yy format&#41; and return the Time and Date formats into their respective text boxes.</p>
-        <form> 
-            First name: <input type="text" onchange="showHint(this.value)"><br><br>
-<!--            Time, Day, Date and Date appear when uploaded-->
-        </form>
-            Time: <br><span id="time"><input name="time" disabled></span><br><br>
-            Day, Date: <br><span id="date"><input name="date" size="20" disabled></span><br><br>
-            Date: <br><span id="datetwo"><input name="datetwo" disabled></span><br><br>
+// <!DOCTYPE html>
+// <html lang="en">
+// <head> 
+// </head>
+// <body>
+//     <p>This webpage contains a form and an Ajax script that will receive data sent from the server. The form contains four input text boxes, for Name, Time, Date&#40;day, date format&#41;, and Date&#40;mm/dd/yy format&#41;. After the user enters their name in the Name text box and then clicks outside this text box, the Ajax script should access three php scripts on the server which will contain the Time, Date&#40;day, date format&#41;, and Date&#40;mm/dd/yy format&#41; and return the Time and Date formats into their respective text boxes.</p>
+//         <form> 
+//             First name: <input type="text" onchange="showHint(this.value)"><br><br>
+// <!--            Time, Day, Date and Date appear when uploaded-->
+//         </form>
+//             Time: <br><span id="time"><input name="time" disabled></span><br><br>
+//             Day, Date: <br><span id="date"><input name="date" size="20" disabled></span><br><br>
+//             Date: <br><span id="datetwo"><input name="datetwo" disabled></span><br><br>
         <script>
             function showHint(str) {
             //time    
@@ -57,5 +57,5 @@
                 }    
             }
     </script> 
-</body>    
-</html>
+// </body>    
+// </html>
