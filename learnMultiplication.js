f826b7537fddd503103f19d3598a352724bdc4a7
ericaandrews <eureka.web.dev@gmail.com>
2018-09-09 18:04:46 -0700

    Update learnMultiplication.js

--- learnMultiplication.js
+++ learnMultiplication.js
@@ -1,14 +1,14 @@
-<!DOCTYPE html>
-<html>
-<head>
-</head>    
-<body>
-    <p>'Computers are playing an increasing role in education. The following webpage contains: a program that will help an elementary school student learn multiplication.'</p><br>
-            <form>
-                <table>     
-                    <tr><td>How much is <span id="randomnumone"></span> times <span id="randomnumtwo"></span>?</td></tr>
-                     <tr><td><input type="number" id="answer"></td></tr>
-                     <tr><td><button type="button" onclick="calculate()">Submit</button><button type="reset">Reset</button><br><br><br></td></tr> 
+// <!DOCTYPE html>
+// <html>
+// <head>
+// </head>    
+// <body>
+//     <p>'Computers are playing an increasing role in education. The following webpage contains: a program that will help an elementary school student learn multiplication.'</p><br>
+//             <form>
+//                 <table>     
+//                     <tr><td>How much is <span id="randomnumone"></span> times <span id="randomnumtwo"></span>?</td></tr>
+//                      <tr><td><input type="number" id="answer"></td></tr>
+//                      <tr><td><button type="button" onclick="calculate()">Submit</button><button type="reset">Reset</button><br><br><br></td></tr> 
                 <script>
                  let randomnumone = document.getElementById("randomnumone").innerHTML = Math.floor(Math.random() * 10);
                  let randomnumtwo = document.getElementById("randomnumtwo").innerHTML = Math.floor(Math.random() * 10);
@@ -42,7 +42,7 @@
                     }
                 }
                 </script>
-            </table>
-        </form>
- </body> 
-</html>    
+//             </table>
+//         </form>
+//  </body> 
+// </html>    
