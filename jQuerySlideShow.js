1c16e69f88de948c74b088b1fe352d1eb3ee856d
ericaandrews <eureka.web.dev@gmail.com>
2018-09-09 18:03:41 -0700

    Update jQuerySlideShow.js

--- jQuerySlideShow.js
+++ jQuerySlideShow.js
@@ -1,147 +1,147 @@
-<!DOCTYPE html>
-<html>
-<head> 
-<style>
-body {
-  margin: 0;
-}
+// <!DOCTYPE html>
+// <html>
+// <head> 
+// <style>
+// body {
+//   margin: 0;
+// }
 
-* {
-  box-sizing: border-box;
-}
+// * {
+//   box-sizing: border-box;
+// }
 
-img {
-  vertical-align: middle;
-}
+// img {
+//   vertical-align: middle;
+// }
 
-/* Position the image container (needed to position the left and right arrows) */
-.container {
-  position: relative;
-}
+// /* Position the image container (needed to position the left and right arrows) */
+// .container {
+//   position: relative;
+// }
 
-/* Hide the images by default */
-.mySlides {
-  display: none;
-}
+// /* Hide the images by default */
+// .mySlides {
+//   display: none;
+// }
 
-/* Add a pointer when hovering over the thumbnail images */
-.cursor {
-  cursor: pointer;
-}
+// /* Add a pointer when hovering over the thumbnail images */
+// .cursor {
+//   cursor: pointer;
+// }
 
-/* Next & previous buttons */
-.prev,
-.next {
-  cursor: pointer;
-  position: absolute;
-  top: 40%;
-  width: auto;
-  padding: 16px;
-  margin-top: -50px;
-  color: white;
-  font-weight: bold;
-  font-size: 20px;
-  border-radius: 0 3px 3px 0;
-  user-select: none;
-  -webkit-user-select: none;
-}
+// /* Next & previous buttons */
+// .prev,
+// .next {
+//   cursor: pointer;
+//   position: absolute;
+//   top: 40%;
+//   width: auto;
+//   padding: 16px;
+//   margin-top: -50px;
+//   color: white;
+//   font-weight: bold;
+//   font-size: 20px;
+//   border-radius: 0 3px 3px 0;
+//   user-select: none;
+//   -webkit-user-select: none;
+// }
 
-/* Position the "next button" to the right */
-.next {
-  right: 0;
-  border-radius: 3px 0 0 3px;
-}
+// /* Position the "next button" to the right */
+// .next {
+//   right: 0;
+//   border-radius: 3px 0 0 3px;
+// }
 
-/* On hover, add a black background color with a little bit see-through */
-.prev:hover,
-.next:hover {
-  background-color: rgba(0, 0, 0, 0.8);
-}
+// /* On hover, add a black background color with a little bit see-through */
+// .prev:hover,
+// .next:hover {
+//   background-color: rgba(0, 0, 0, 0.8);
+// }
 
-/* Number text (1/3 etc) */
-.numbertext {
-  color: #f2f2f2;
-  font-size: 12px;
-  padding: 8px 12px;
-  position: absolute;
-  top: 0;
-}
+// /* Number text (1/3 etc) */
+// .numbertext {
+//   color: #f2f2f2;
+//   font-size: 12px;
+//   padding: 8px 12px;
+//   position: absolute;
+//   top: 0;
+// }
 
-/* Container for image text */
-.caption-container {
-  text-align: center;
-  background-color: #222;
-  padding: 2px 16px;
-  color: white;
-}
+// /* Container for image text */
+// .caption-container {
+//   text-align: center;
+//   background-color: #222;
+//   padding: 2px 16px;
+//   color: white;
+// }
 
-.row:after {
-  content: "";
-  display: table;
-  clear: both;
-}
+// .row:after {
+//   content: "";
+//   display: table;
+//   clear: both;
+// }
 
-/* Six columns side by side */
-.column {
-  float: left;
-  width: 25.00%;
-}
+// /* Six columns side by side */
+// .column {
+//   float: left;
+//   width: 25.00%;
+// }
 
-/* Add a transparency effect for thumnbail images */
-.demo {
-  opacity: 0.6;
-}
+// /* Add a transparency effect for thumnbail images */
+// .demo {
+//   opacity: 0.6;
+// }
 
-.active,
-.demo:hover {
-  opacity: 1;
-}
-</style>    
-</head>
-<!--This webpage contains a slide-show using jQuery.    -->
-<body>
-<div id="border">
-<div class="container">
-  <div class="mySlides">
-    <div class="numbertext">1 / 4</div>
-    <img src="IMG_3265.jpg" style="width:100%">
-  </div>
+// .active,
+// .demo:hover {
+//   opacity: 1;
+// }
+// </style>    
+// </head>
+// <!--This webpage contains a slide-show using jQuery.    -->
+// <body>
+// <div id="border">
+// <div class="container">
+//   <div class="mySlides">
+//     <div class="numbertext">1 / 4</div>
+//     <img src="IMG_3265.jpg" style="width:100%">
+//   </div>
 
-  <div class="mySlides">
-    <div class="numbertext">2 / 4</div>
-    <img src="IMG_3267.jpg" style="width:100%">
-  </div>
+//   <div class="mySlides">
+//     <div class="numbertext">2 / 4</div>
+//     <img src="IMG_3267.jpg" style="width:100%">
+//   </div>
 
-  <div class="mySlides">
-    <div class="numbertext">3 / 4</div>
-    <img src="IMG_3270.jpg" style="width:100%">
-  </div>
+//   <div class="mySlides">
+//     <div class="numbertext">3 / 4</div>
+//     <img src="IMG_3270.jpg" style="width:100%">
+//   </div>
     
-  <div class="mySlides">
-    <div class="numbertext">4 / 4</div>
-    <img src="IMG_3269.jpg" style="width:100%">
-  </div>  
-  <a class="prev" onclick="plusSlides(-1)">❮</a>
-  <a class="next" onclick="plusSlides(1)">❯</a>
-  <div class="caption-container">
-    <p id="caption"></p>
-  </div>
+//   <div class="mySlides">
+//     <div class="numbertext">4 / 4</div>
+//     <img src="IMG_3269.jpg" style="width:100%">
+//   </div>  
+//   <a class="prev" onclick="plusSlides(-1)">❮</a>
+//   <a class="next" onclick="plusSlides(1)">❯</a>
+//   <div class="caption-container">
+//     <p id="caption"></p>
+//   </div>
 
-  <div class="row">
-    <div class="column">
-      <img class="demo cursor" src="IMG_3265.jpg" style="width:100%" onclick="currentSlide(1)">
-    </div>
-    <div class="column">
-      <img class="demo cursor" src="IMG_3267.jpg" style="width:100%" onclick="currentSlide(2)">
-    </div>
-    <div class="column">
-      <img class="demo cursor" src="IMG_3270.jpg" style="width:100%" onclick="currentSlide(3)">
-    </div>
-    <div class="column">
-      <img class="demo cursor" src="IMG_3269.jpg" style="width:100%" onclick="currentSlide(4)">
-    </div>
-  </div>
-</div>
+//   <div class="row">
+//     <div class="column">
+//       <img class="demo cursor" src="IMG_3265.jpg" style="width:100%" onclick="currentSlide(1)">
+//     </div>
+//     <div class="column">
+//       <img class="demo cursor" src="IMG_3267.jpg" style="width:100%" onclick="currentSlide(2)">
+//     </div>
+//     <div class="column">
+//       <img class="demo cursor" src="IMG_3270.jpg" style="width:100%" onclick="currentSlide(3)">
+//     </div>
+//     <div class="column">
+//       <img class="demo cursor" src="IMG_3269.jpg" style="width:100%" onclick="currentSlide(4)">
+//     </div>
+//   </div>
+// </div>
 
 <script>
 var slideIndex = 1;
@@ -173,6 +173,6 @@ function showSlides(n) {
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
 </script>
-</div>
-</body>    
-</html>
+// </div>
+// </body>    
+// </html>
