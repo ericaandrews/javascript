8be3714cc559b924d5f092e10833be0e2c1fe8ff
ericaandrews <eureka.web.dev@gmail.com>
2018-09-09 17:55:56 -0700

    Update documentWriteStatements.js

--- documentWriteStatements.js
+++ documentWriteStatements.js
@@ -1,13 +1,13 @@
-<!DOCTYPE html>
-<html>
-<head>  
-</head>    
-<body>
-<!--        Document Write Statements-->
+// <!DOCTYPE html>
+// <html>
+// <head>  
+// </head>    
+// <body>
+// <!--        Document Write Statements-->
         <script>
         document.write("ab ");
         document.write("12 ");
         document.write("34 ");    
         </script> 
-</body> 
-</html>    
+// </body> 
+// </html>    
