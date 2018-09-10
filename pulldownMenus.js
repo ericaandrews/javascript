54e0f5a7bbcd6df521330837f3fcda8a1a1aee40
ericaandrews <eureka.web.dev@gmail.com>
2018-09-09 18:28:54 -0700

    Update pulldownMenus.js

--- pulldownMenus.js
+++ pulldownMenus.js
@@ -1,68 +1,68 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<style>
-.button, .buttons {
-    background-color: #ddd;
-    padding: 8px;
-    font-size: 12px;
-    border: none;
-    border-radius: 6px;
-}
-.button:hover, .button:focus, .buttons:hover, .buttons:focus {
-    background-color: #2980B9;
-}
-.pulldown, .pulldowns {
-    position: relative;
-    display: inline-block;
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+// <style>
+// .button, .buttons {
+//     background-color: #ddd;
+//     padding: 8px;
+//     font-size: 12px;
+//     border: none;
+//     border-radius: 6px;
+// }
+// .button:hover, .button:focus, .buttons:hover, .buttons:focus {
+//     background-color: #2980B9;
+// }
+// .pulldown, .pulldowns {
+//     position: relative;
+//     display: inline-block;
 
-}
-.content, .contents {
-    display: none;
-    position: absolute;
-    background-color: #eee;
-    min-width: 160px;
-    overflow: auto;
-    border-radius: 10px;
-    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-    z-index: 1;
-}
-.content a, .contents a {
-    padding: 12px 12px;
-    text-decoration: none;
-    display: block;
-    color: #000;
-}
-.pulldown a:hover, .pulldowns a:hover {background-color: #ddd}
-.links, .linkss {display:block;}
-p{font-size: 8pt;}    
-</style>
-</head>
-<body>
-<div style="text-align: center">
-<br><br><br>   
-<p style="font-size: 11pt">This webpage contains a script that utilizes 2 pulldown menus. The first one utilizes an onchange event handler. The second utilizes a button with an onclick event handler to navigate to a page after an option is selected.</p>      
-<p>Onclick Event Handler</p>
-<div class="pulldown">
-<button onclick="myFunction()" class="button">Pull Down Menu</button>
-  <div id="onClick" class="content">
-    <a href="https://en.wikipedia.org/wiki/JavaScript" target="_blank">Wikipedia: Javascript</a>
-    <a href="https://en.wikipedia.org/wiki/HTML" target="_blank">Wikipedia: HTML</a>
-    <a href="https://en.wikipedia.org/wiki/Cascading_Style_Sheets" target="_blank">Wikipedia: CSS</a>
-    <a href="https://en.wikipedia.org/wiki/Array_data_type" target="_blank">Wikipedia: Array</a>
-  </div>
-</div>
-<br><br>    
-<p>Onchange Event Handler</p>
-    <div class="pulldowns">
-    <select onchange="window.open(this.options[this.selectedIndex].value)" class="buttons" id="onChange">
-    <option>Please select from the following: </option>    
-    <option value="https://en.wikipedia.org/wiki/JavaScript">Wikipedia: Javascript</option>
-    <option value="https://en.wikipedia.org/wiki/HTML">Wikipedia: HTML</option>
-    <option value="https://en.wikipedia.org/wiki/Cascading_Style_Sheets">Wikipedia: CSS</option>
-    <option value="https://en.wikipedia.org/wiki/Array_data_type">Wikipedia: Array</option>
-    </select>    
-    </div>    
+// }
+// .content, .contents {
+//     display: none;
+//     position: absolute;
+//     background-color: #eee;
+//     min-width: 160px;
+//     overflow: auto;
+//     border-radius: 10px;
+//     box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+//     z-index: 1;
+// }
+// .content a, .contents a {
+//     padding: 12px 12px;
+//     text-decoration: none;
+//     display: block;
+//     color: #000;
+// }
+// .pulldown a:hover, .pulldowns a:hover {background-color: #ddd}
+// .links, .linkss {display:block;}
+// p{font-size: 8pt;}    
+// </style>
+// </head>
+// <body>
+// <div style="text-align: center">
+// <br><br><br>   
+// <p style="font-size: 11pt">This webpage contains a script that utilizes 2 pulldown menus. The first one utilizes an onchange event handler. The second utilizes a button with an onclick event handler to navigate to a page after an option is selected.</p>      
+// <p>Onclick Event Handler</p>
+// <div class="pulldown">
+// <button onclick="myFunction()" class="button">Pull Down Menu</button>
+//   <div id="onClick" class="content">
+//     <a href="https://en.wikipedia.org/wiki/JavaScript" target="_blank">Wikipedia: Javascript</a>
+//     <a href="https://en.wikipedia.org/wiki/HTML" target="_blank">Wikipedia: HTML</a>
+//     <a href="https://en.wikipedia.org/wiki/Cascading_Style_Sheets" target="_blank">Wikipedia: CSS</a>
+//     <a href="https://en.wikipedia.org/wiki/Array_data_type" target="_blank">Wikipedia: Array</a>
+//   </div>
+// </div>
+// <br><br>    
+// <p>Onchange Event Handler</p>
+//     <div class="pulldowns">
+//     <select onchange="window.open(this.options[this.selectedIndex].value)" class="buttons" id="onChange">
+//     <option>Please select from the following: </option>    
+//     <option value="https://en.wikipedia.org/wiki/JavaScript">Wikipedia: Javascript</option>
+//     <option value="https://en.wikipedia.org/wiki/HTML">Wikipedia: HTML</option>
+//     <option value="https://en.wikipedia.org/wiki/Cascading_Style_Sheets">Wikipedia: CSS</option>
+//     <option value="https://en.wikipedia.org/wiki/Array_data_type">Wikipedia: Array</option>
+//     </select>    
+//     </div>    
 <script>
 
 function myFunction() {
@@ -82,6 +82,6 @@ window.onclick = function(event) {
   }
 }
 </script>
- </div> 
-</body>
-</html>
+//  </div> 
+// </body>
+// </html>
