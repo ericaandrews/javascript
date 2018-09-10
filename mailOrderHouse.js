fd6c81cc368cf8e3c86d9b7593396e0b8f3a732a
ericaandrews <eureka.web.dev@gmail.com>
2018-09-09 18:16:04 -0700

    Update mailOrderHouse.js

--- mailOrderHouse.js
+++ mailOrderHouse.js
@@ -1,25 +1,25 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <style>   
-      #p_mail, #p_order, #p_square{text-align: center;}
-      table, th, td{border: 1px solid;font-size: 8pt; margin: 0 auto; border-collapse: collapse;}
-    </style>
-</head>    
-<body>
-    <p id="p_mail">This webpage calculates an amount sold weekly based on quantity and price of five products sold by a mail-order house.</p>
-        <p id="p_order">Enter quantity for each product:</p><br>
-            <form name="mailorder">
-            <table>
-                <tr id="background_one"><td>Product #</td><td>Qty</td><td>Price</td><td>Amount</td></tr>
-                <tr id="background_two"><td><input value="1" id="one" disabled></td><td><input id="qtyone" type="number"></td><td>2.98</td><td><input id="amntone" readonly></td></tr>
-                <tr id="background_three"><td><input value="2" id="two" disabled></td><td><input id="qtytwo" type="number"></td><td>4.50</td><td><input id="amnttwo" readonly></td></tr>
-                <tr id="background_four"><td><input value="3" id="three" disabled></td><td><input id="qtythree" type="number"></td><td>9.98</td><td><input id="amntthree" readonly></td></tr>
-                <tr id="background_five"><td><input value="4" id="four" disabled></td><td><input id="qtyfour" type="number"></td><td>4.49</td><td><input id="amntfour" readonly></td></tr>
-                <tr id="background_six"><td><input value="5" id="five" disabled></td><td><input id="qtyfive" type="number"></td><td>6.87</td><td><input id="amntfive" readonly></td></tr>
-                <tr id="background_seven"><td colspan="3">Total Amount Sold</td><td><input id="totlamnt" readonly></td></tr>
-                <tr id="background_eight"><td colspan="4"><button type="button" onclick="amount();">Submit</button><button type="reset">Reset</button></td></tr>
-            </table>
+// <!DOCTYPE html>
+// <html>
+// <head>
+//     <style>   
+//       #p_mail, #p_order, #p_square{text-align: center;}
+//       table, th, td{border: 1px solid;font-size: 8pt; margin: 0 auto; border-collapse: collapse;}
+//     </style>
+// </head>    
+// <body>
+//     <p id="p_mail">This webpage calculates an amount sold weekly based on quantity and price of five products sold by a mail-order house.</p>
+//         <p id="p_order">Enter quantity for each product:</p><br>
+//             <form name="mailorder">
+//             <table>
+//                 <tr id="background_one"><td>Product #</td><td>Qty</td><td>Price</td><td>Amount</td></tr>
+//                 <tr id="background_two"><td><input value="1" id="one" disabled></td><td><input id="qtyone" type="number"></td><td>2.98</td><td><input id="amntone" readonly></td></tr>
+//                 <tr id="background_three"><td><input value="2" id="two" disabled></td><td><input id="qtytwo" type="number"></td><td>4.50</td><td><input id="amnttwo" readonly></td></tr>
+//                 <tr id="background_four"><td><input value="3" id="three" disabled></td><td><input id="qtythree" type="number"></td><td>9.98</td><td><input id="amntthree" readonly></td></tr>
+//                 <tr id="background_five"><td><input value="4" id="four" disabled></td><td><input id="qtyfour" type="number"></td><td>4.49</td><td><input id="amntfour" readonly></td></tr>
+//                 <tr id="background_six"><td><input value="5" id="five" disabled></td><td><input id="qtyfive" type="number"></td><td>6.87</td><td><input id="amntfive" readonly></td></tr>
+//                 <tr id="background_seven"><td colspan="3">Total Amount Sold</td><td><input id="totlamnt" readonly></td></tr>
+//                 <tr id="background_eight"><td colspan="4"><button type="button" onclick="amount();">Submit</button><button type="reset">Reset</button></td></tr>
+//             </table>
             <script>
             function amount(){
                 
@@ -94,7 +94,7 @@
                 document.getElementById("totlamnt").value = sum.toFixed(2);        
             }
             </script>  
-            </form>
-        <br>
-</body> 
-</html>    
+//             </form>
+//         <br>
+// </body> 
+// </html>    
