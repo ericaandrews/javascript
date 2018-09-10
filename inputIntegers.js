21059eda77826f71aacbb460d1c80303a228d7f5
ericaandrews <eureka.web.dev@gmail.com>
2018-09-09 18:01:24 -0700

    Update inputIntegers.js

--- inputIntegers.js
+++ inputIntegers.js
@@ -1,21 +1,21 @@
-<!DOCTYPE html>
-<html>
-<head>
-</head>    
-<!--Enter three integers to receive a sum, average, product, lowest value and highest value    -->
-<body style="text-align: right">
-    <form>    
-        Integer one: <input type="number" name="one" /><br>  
-        Integer two: <input type="number" name="two"/><br>   
-        Integer three: <input type="number" name="three"/><br><br>          
-        <input type="button" value="Submit" onclick="sum_int(); average_int(); product_int(); smallest_int(); largest_int();"/>
-        <input type="reset" value="Reset"/><br><br>    
-            Sum: <input type="number" name="sum" readonly/><br> 
-            Average: <input type="number" name="average" readonly/><br>    
-            Product: <input type="number" name="product" readonly/><br>
-            Smallest: <input type="number" name="smallest" readonly/><br>  
-            Largest: <input type="number" name="largest" readonly/>    
-    </form>
+// <!DOCTYPE html>
+// <html>
+// <head>
+// </head>    
+// <!--Enter three integers to receive a sum, average, product, lowest value and highest value    -->
+// <body style="text-align: right">
+//     <form>    
+//         Integer one: <input type="number" name="one" /><br>  
+//         Integer two: <input type="number" name="two"/><br>   
+//         Integer three: <input type="number" name="three"/><br><br>          
+//         <input type="button" value="Submit" onclick="sum_int(); average_int(); product_int(); smallest_int(); largest_int();"/>
+//         <input type="reset" value="Reset"/><br><br>    
+//             Sum: <input type="number" name="sum" readonly/><br> 
+//             Average: <input type="number" name="average" readonly/><br>    
+//             Product: <input type="number" name="product" readonly/><br>
+//             Smallest: <input type="number" name="smallest" readonly/><br>  
+//             Largest: <input type="number" name="largest" readonly/>    
+//     </form>
         <script>
             function sum_int(){
                 let one = document.getElementsByName("one")[0].value;
@@ -53,5 +53,5 @@
                 document.getElementsByName("largest")[0].value = largest;
             }    
         </script>
-   </body> 
-</html>    
+//    </body> 
+// </html>    
